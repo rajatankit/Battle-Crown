88522acd4c640a8c029b7a6341508afffdf69363
Rajat Ankit <rajatyn63@gmail.com>
2026-08-08 02:05:55 +0530

    fix path

--- app/api/match/upload-ss/route.js
+++ app/api/match/upload-ss/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { prisma } from "../../../../lib/prisma";
+import { prisma } from "../../../lib/prisma";
 import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
