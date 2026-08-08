e08ba70c52bf0c8f26d09e47a99b43639a3acc42
Rajat Ankit <rajatyn63@gmail.com>
2026-08-08 15:07:34 +0530

    fix firebase admin runtime

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
-/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig}
+ * 
+ */
 const nextConfig = {
-  /* config options here */
+  serverExternalPackages:
+   ["firebase-admin"],
 };
 
 export default nextConfig;
